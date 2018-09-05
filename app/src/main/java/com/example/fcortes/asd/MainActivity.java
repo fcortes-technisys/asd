0bd06c94059a2fe63a6363128a9b4aa16d2b5043
fernandocortes10 <fernando.cortes@live.com>
2018-09-05 17:26:33 -0300

    avances

--- app/src/main/java/com/example/fcortes/asd/MainActivity.java
+++ app/src/main/java/com/example/fcortes/asd/MainActivity.java
@@ -1,27 +1,17 @@
 package com.example.fcortes.asd;
-
-import android.provider.SyncStateContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 
-import com.scottyab.aescrypt.AESCrypt;
-
 import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import github.didikee.aes256cbc.AES256Util;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,59 +20,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // encriptacion de pass
-
-        byte[] encodedBytes = Base64.encode(getHash("MDAyX0FDSWNobGtxWXI4eFFvQzhCMloxUFBhV2FjM0hKWGR2eDFPT1Z0U2VTL2hFSTBvOHIzdVRDTGFLbW1wYjAzK2M5Qkk5WVMrZ080UXkNCjM5d1c3MDlEcjJ1MUgvQi9SUjdKMGdjVjhON1hTNzQ9DQo"), Base64.DEFAULT);
-        Log.i("encodedBytes " , new String(encodedBytes));
-        String key = new String(encodedBytes);
-        Log.i("encodedBytes key" , key);
-
-
-
-        byte[]  b = new byte[0];
         try {
-            b = AES256Util.encrypt(key.substring(0, 32), "0000000000000000", "user_password".getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
+            Log.i("lala aaaaaaaaaa " , new String(Base64.encode(encrypt("1234", "9f3SlY6tPLYW429RHrAJUsFSpIDS0QowTue Sep 04 16:58:26 GMT-03:00 2018"), Base64.DEFAULT)));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        Log.i("AES256Util encoded" , new String(Base64.encode(b, Base64.DEFAULT)));
-        String encoded = new String(Base64.encode(b, Base64.DEFAULT));
+
+        // encriptacion de pass
+
+        byte[] encodedBytes = getHash("9f3SlY6tPLYW429RHrAJUsFSpIDS0QowTue Sep 04 16:58:26 GMT-03:00 2018");
+        Log.i("lala encodedBytes " , new String(encodedBytes));
+        String key = new String(encodedBytes);
+        Log.i("lala encodedBytes key" , String.valueOf(encodedBytes.length));
+
+
+//        byte[]  b = new byte[0];
+//        try {
+//            b = AES256Util.encrypt(key.substring(0, 32), "0000000000000000", "user_password".getBytes("UTF-8"));
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//        Log.i("AES256Util encoded" , new String(Base64.encode(b, Base64.DEFAULT)));
+//        String encoded = new String(Base64.encode(b, Base64.DEFAULT));
 
 
         // decriptacion ?
 
         try {
-            String encrytedData = encrytData("user_password", key.substring(0, 32));
-            Log.i("AES256Util encoded" , encrytedData);
-            Log.i("AES256Util decoded" , decryptData(encrytedData, key.substring(0, 32)));
+            String encrytedData = encrytData("1234", encodedBytes);
+            Log.i("lala AES256Util encoded" , encrytedData);
+            Log.i("lala AES256Util decoded" , decryptData(encrytedData, encodedBytes));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static String encrytData(String text, String key) throws Exception {
+    public static String encrytData(String text,  byte[] encodedBytes) throws Exception {
 
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
-        byte[] static_key = key.getBytes("UTF-8");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+//        byte[] static_key = key.getBytes("UTF-8");
 
-        SecretKeySpec keySpec = new SecretKeySpec(static_key, "AES");
+        SecretKeySpec keySpec = new SecretKeySpec(encodedBytes, "AES");
         IvParameterSpec ivSpec = new IvParameterSpec("0000000000000000".getBytes("UTF-8"));
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 
-        byte[] results = cipher.doFinal(text.getBytes());
+        byte[] results = cipher.doFinal(text.getBytes("UTF-8"));
 
-        return Base64.encodeToString(results, Base64.NO_WRAP);
+//        return new String(results);
+        return new String(Base64.encode(results, Base64.DEFAULT));
 
     }
 
-    public static String decryptData(String text, String key) throws Exception{
+    public static String decryptData(String text, byte[] key) throws Exception{
 
         byte[] encryted_bytes = Base64.decode(text, Base64.DEFAULT);
 
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
-        byte[] static_key = key.getBytes("UTF-8");
+//        byte[] static_key = key.getBytes("UTF-8");
 
-        SecretKeySpec keySpec = new SecretKeySpec(static_key, "AES");
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
         IvParameterSpec ivSpec = new IvParameterSpec("0000000000000000".getBytes("UTF-8"));
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
 
@@ -93,13 +89,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public byte[] getHash(String password) {
-        MessageDigest digest=null;
+        byte[] keyBytes = null;
         try {
-            digest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e1) {
+            int keySize = 32;
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(password.getBytes("UTF-8"));
+            keyBytes = new byte[keySize];
+            System.arraycopy(Base64.encode(digest.digest(), Base64.DEFAULT), 0, keyBytes, 0, 32);
+
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e1) {
             e1.printStackTrace();
         }
-        digest.reset();
-        return digest.digest(password.getBytes());
+
+
+        return keyBytes;
     }
+
+
+
+
+    public static byte[] encrypt(String plainText, String key) throws Exception {
+        byte[] clean = plainText.getBytes();
+
+        int ivSize = 16;
+        int keySize = 32;
+
+        // Generating IV.
+        byte[] iv = new byte[ivSize];
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        // Hashing key.
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(key.getBytes("UTF-8"));
+        byte[] keyBytes = new byte[keySize];
+
+        System.arraycopy(Base64.encode(digest.digest(), Base64.DEFAULT), 0, keyBytes, 0, keyBytes.length);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Encrypt.
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] encrypted = cipher.doFinal(clean);
+
+        return encrypted;
+    }
+
 }
